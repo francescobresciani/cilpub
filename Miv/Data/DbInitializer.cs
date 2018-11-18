fe9f22275fb3040126c6eb8016e867cd38434ecc
frab <francesco.bresciani@student.supsi.ch>
2018-11-18 13:01:22 +0100

    db initializer ok

--- Miv/Data/DbInitializer.cs
+++ Miv/Data/DbInitializer.cs
@@ -24,10 +24,10 @@ namespace Miv.Data
                 new Material{MaterialID=1,Name="parent 1",Description="i have 3 children",imgUrl="images/01.jpg"},
                 new Material{MaterialID=2,Name="parent 2",Description="i have 2 children",imgUrl="images/02.jpg"},
                 new Material{MaterialID=3,Name="parent 3",Description="i have no children",imgUrl="images/03.jpg"},
-                new Material{MaterialID=10,Name="1st child of 1",Description="qweqwe",imgUrl="images/04.jpg"},
-                new Material{MaterialID=11,Name="2nd child of 1",Description="sddbafwefwf",imgUrl="images/05.jpg"},
-                new Material{MaterialID=12,Name="3rd child of 1",Description="sddbafwefwf",imgUrl="images/06.jpg"},
-                new Material{MaterialID=21,Name="only child of 2",Description="sddbafwefwf",imgUrl="images/07.jpg"}
+                new Material{MaterialID=10,Name="1st child of 1",Description="i am a child",imgUrl="images/04.jpg"},
+                new Material{MaterialID=11,Name="2nd child of 1",Description="i am a child",imgUrl="images/05.jpg"},
+                new Material{MaterialID=12,Name="3rd child of 1",Description="i am a child",imgUrl="images/06.jpg"},
+                new Material{MaterialID=21,Name="only child of 2",Description="i am a child",imgUrl="images/07.jpg"}
             };
             foreach (Material m in materials)
             {
